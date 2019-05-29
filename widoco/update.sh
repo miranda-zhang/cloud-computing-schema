cde715222a005c1639ff8ab7c5187aa5e8080d37
u5214628 <miranda.zhang@anu.edu.au>
2019-05-29 10:43:53 +1000

    move SystemImage under ComputeService

--- widoco/update.sh
+++ widoco/update.sh
@@ -3,9 +3,11 @@
 # set -o xtrace
 
 # -------------------------------Configurable Parameters-------------------------------------------
-# version="1.0.1"
-version="0.1"
+
+
 previous_version="0.1"
+# version="0.1"
+version="1.0.1"
 
 # backword slash for windows style dir, put qoutation around
 ontFile='C:\Users\admin-u5214628\Documents\cloud-computing-schema\ontology_dev\cocoon'$version'.ttl'
